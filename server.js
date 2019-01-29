e071bb1613d1a59cb8a805dfe379016a5e2e11f5
Zaniaf <nickelargy91@gmail.com>
2019-01-29 23:48:56 +0100

    url

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const imageurl = require('./controllers/image');
 
 const db = knex({
   client: 'pg',
